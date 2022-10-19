b76cb780dceda86d34b60a0bb47cff22c6d1fd4b
Duygu Eroğlu <45760467+duygueroglu@users.noreply.github.com>
2022-10-19 23:02:35 +0300

    Kanban.tsx page updated

--- src/pages/Kanban.tsx
+++ src/pages/Kanban.tsx
@@ -1,12 +1,22 @@
-import React, { FC } from 'react'
+import React, { FC, useState  } from 'react'
 import { Link } from 'react-router-dom'
 import Button from '../components/Button'
+import { auth } from '../services/http/patika/endpoints/auth'
+import Board from '../components/KanbanEntities//Board'
+import { useBoardContext } from '../contexts/BoardContext/BoardContext'
+import AddBoard from '../components/KanbanEntities/AddBoard'
+import AddButton from '../components/KanbanEntities/AddBoard/AddButton'
 
 export type KanbanProps = {
   onLogout: () => void
   onAddBoard: () => void
 }
 const Kanban: FC<KanbanProps> = (props) => {
+  const {state, setState} = useBoardContext()
+  const [boardFormValues, setBoardFormValues] = useState<BoardFormValuesProps>({
+    title: "Initial Board",
+    members: []
+  })
   const handleLogout = () => {
     props.onLogout?.()
   }
@@ -15,10 +25,18 @@ const Kanban: FC<KanbanProps> = (props) => {
   }
   return (
     <div>
-      <h1>Welcome ${username} to the kanban app</h1>
-      <Button onClick={handleAddBoard}>Add New Board</Button>
-      <Link to='/password-change'><Button>Password Change</Button></Link>
-      <Button onClick={handleLogout}>Logout</Button>
+      <Link to='/board/:id'>
+        <Button style = {{
+          width:'8%', height:'100px', margin:'50px', backgroundColor:'#4f5a7a'
+      }} 
+        onClick={handleAddBoard} type="submit">Add New Board</Button>
+        </Link>
+      <Link to='/password-change'><Button  style = {{
+          width:'8%', height:'100px', margin:'50px', backgroundColor:'#4f5a7a'
+      }}>Password Change</Button></Link>
+      <Button  style = {{
+          width:'8%', height:'100px', margin:'50px', backgroundColor:'#4f5a7a'
+      }} onClick={handleLogout}>Logout</Button>
     </div>
   )
 }
